e229c6b50fb2278e8530bb3d2c88e07022014995
Thiago Senna <trsenna@gmail.com>
2017-01-07 08:53:33 -0200

    added initial theme structure

---
+++ public_html/content/themes/unnamed17/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+
+    <header id="masthead" class="site-header" role="banner">
+        -- site/branding --
+    </header><!-- #masthead -->
+
+    <div id="content" class="site-content">
