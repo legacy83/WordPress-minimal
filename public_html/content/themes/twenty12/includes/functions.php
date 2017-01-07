fc6a73831f1beb28cb4c3be43c977f9c011feb50
Thiago Senna <trsenna@gmail.com>
2017-01-06 22:34:51 -0200

    better comments for theme functionality cleanup

--- public_html/content/themes/twenty12/includes/functions.php
+++ public_html/content/themes/twenty12/includes/functions.php
@@ -12,10 +12,18 @@ add_action('after_setup_theme', function () {
     add_filter('use_default_gallery_style', '__return_false');
 });
 
+// theme support cleanup
+add_action('after_setup_theme', function () {
+    remove_theme_support('custom-background');
+    remove_theme_support('custom-header');
+}, 15);
+
+// nav menu cleanup
 add_action('after_setup_theme', function () {
     unregister_nav_menu('primary');
 }, 15);
 
+// sidebar cleanup
 add_action('widgets_init', function () {
     unregister_sidebar('sidebar-1');
     unregister_sidebar('sidebar-2');
