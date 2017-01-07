e847660f685fa29a37d2736af2b5485da877e489
Thiago Senna <trsenna@gmail.com>
2017-01-07 10:51:36 -0200

    added theme functionality

--- public_html/content/themes/unnamed17/includes/functions.php
+++ public_html/content/themes/unnamed17/includes/functions.php
@@ -6,3 +6,38 @@
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 namespace site\themes\unnamed17;
+
+// registers support for various WordPress features
+add_action('after_setup_theme', function () {
+    add_theme_support('title-tag');
+    add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+});
+
+// set the content width in pixels
+add_action('after_setup_theme', function () {
+    $GLOBALS['content_width'] = 640;
+}, 0);
+
+// enqueue scripts and styles
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('unnamed17-style', get_stylesheet_uri());
+    wp_enqueue_script('unnamed17-global', get_template_directory_uri() . '/assets/js/global.js', array('jquery'), false, true);
+});
+
+// force error 404 for some templates
+add_action('template_redirect', function () {
+    /**
+     * @var $wp_query \WP_Query
+     */
+    global $wp_query;
+
+    if (is_404()) :
+    elseif (is_front_page()) :
+    elseif (is_page()) :
+    else :
+
+        $wp_query->set_404();
+        status_header(404);
+
+    endif;
+});
