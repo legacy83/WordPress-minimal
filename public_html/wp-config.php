be7a303957212e42999df7ce46a8362262af718f
Thiago Senna <trsenna@gmail.com>
2017-01-08 09:10:38 -0200

    overridable wp-config

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -9,12 +9,12 @@
  *
  */
 
-define( 'DB_NAME', 'www' );
-define( 'DB_USER', 'www' );
-define( 'DB_PASSWORD', 'www@secret' );
-define( 'DB_HOST', 'localhost' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+defined('DB_NAME') or define('DB_NAME', 'www');
+defined('DB_USER') or define('DB_USER', 'www');
+defined('DB_PASSWORD') or define('DB_PASSWORD', 'www@secret');
+defined('DB_HOST') or define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 
 /*
@@ -26,8 +26,8 @@ define( 'DB_COLLATE', '' );
  *
  */
 
-defined( 'WP_HOME' ) or define( 'WP_HOME', 'http://www.gozma14.local' );
-defined( 'WP_SITEURL' ) or define( 'WP_SITEURL', 'http://www.gozma14.local/wp' );
+defined('WP_HOME') or define('WP_HOME', 'http://www.gozma14.local');
+defined('WP_SITEURL') or define('WP_SITEURL', 'http://www.gozma14.local/wp');
 
 
 /*
@@ -39,8 +39,8 @@ defined( 'WP_SITEURL' ) or define( 'WP_SITEURL', 'http://www.gozma14.local/wp' )
  *
  */
 
-defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-defined( 'WP_CONTENT_URL' ) or define( 'WP_CONTENT_URL', WP_HOME . '/content' );
+defined('WP_CONTENT_DIR') or define('WP_CONTENT_DIR', dirname(__FILE__) . '/content');
+defined('WP_CONTENT_URL') or define('WP_CONTENT_URL', WP_HOME . '/content');
 
 
 /*
@@ -54,14 +54,14 @@ defined( 'WP_CONTENT_URL' ) or define( 'WP_CONTENT_URL', WP_HOME . '/content' );
  *
  */
 
-define('AUTH_KEY',         'd(|^$LYy|@,tm|Q4)L5[9&&@?y]s;ybL!:UJow+nv+6>IFjfh}v)ca6*#zF-n--h');
-define('SECURE_AUTH_KEY',  '}QfJ|ft7k?O6bKizeFeQ/[#o2)xU@!t<H4*jVC)|svIfCBw_ASlN#?/XO4aj_&V&');
-define('LOGGED_IN_KEY',    'FFfB*&-#f<3,Ev|sd1CPi-~h(}]AAEi=*[{@ r=%TiH+OIi+.2]t$ 8o*_f#.AOm');
-define('NONCE_KEY',        'u*i-T%Mv@NF iIc<^=2d~=/lb8SQh]aYsl;[dr-m!c6|{17K) 6p`jL/nthKo;jN');
-define('AUTH_SALT',        'Qby{+:|Bq4%|g@=zE+g++_%ONnq>I^(a!}YnP2d .y3]YeY0]v!x3bn&9Gn!gi25');
+define('AUTH_KEY', 'd(|^$LYy|@,tm|Q4)L5[9&&@?y]s;ybL!:UJow+nv+6>IFjfh}v)ca6*#zF-n--h');
+define('SECURE_AUTH_KEY', '}QfJ|ft7k?O6bKizeFeQ/[#o2)xU@!t<H4*jVC)|svIfCBw_ASlN#?/XO4aj_&V&');
+define('LOGGED_IN_KEY', 'FFfB*&-#f<3,Ev|sd1CPi-~h(}]AAEi=*[{@ r=%TiH+OIi+.2]t$ 8o*_f#.AOm');
+define('NONCE_KEY', 'u*i-T%Mv@NF iIc<^=2d~=/lb8SQh]aYsl;[dr-m!c6|{17K) 6p`jL/nthKo;jN');
+define('AUTH_SALT', 'Qby{+:|Bq4%|g@=zE+g++_%ONnq>I^(a!}YnP2d .y3]YeY0]v!x3bn&9Gn!gi25');
 define('SECURE_AUTH_SALT', 'l/ LO9pnOB@c!LJ6t*Q83e5<n,h/sWg$j40uf#+)1XadOFLt56R,f|Ws=fGQ6ZG)');
-define('LOGGED_IN_SALT',   'Nd-&rf[@4v]]f+{@)zBXh>Kqrs,,0LAWWy-]q6`Nj^PpV(EMsxc$bi#F&??u0/Ya');
-define('NONCE_SALT',       'NY&g>#Yf*V6y0PQ4:s5C3_bwdNTZ`y/D3<q((|9=+dsOxJ}~o00u%1$i_%-EEO$%');
+define('LOGGED_IN_SALT', 'Nd-&rf[@4v]]f+{@)zBXh>Kqrs,,0LAWWy-]q6`Nj^PpV(EMsxc$bi#F&??u0/Ya');
+define('NONCE_SALT', 'NY&g>#Yf*V6y0PQ4:s5C3_bwdNTZ`y/D3<q((|9=+dsOxJ}~o00u%1$i_%-EEO$%');
 
 
 /*
@@ -93,21 +93,7 @@ $table_prefix = 'wp_';
  *
  */
 
-define( 'WP_DEBUG', FALSE );
-
-
-/*
- * --------------------------------------------------------------------------------
- * Absolute path
- * --------------------------------------------------------------------------------
- *
- * Absolute path to the WordPress directory.
- *
- */
-
-if ( !defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
-}
+defined('WP_DEBUG') or define('WP_DEBUG', FALSE);
 
 
 /*
@@ -119,4 +105,8 @@ if ( !defined( 'ABSPATH' ) ) {
  *
  */
 
-require_once( ABSPATH . 'wp-settings.php' );
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/wp/');
+}
+
+require_once(ABSPATH . 'wp-settings.php');
