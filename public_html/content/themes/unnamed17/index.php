57d31814a991810f6fc32ddaa5116968e00b899f
Thiago Senna <trsenna@gmail.com>
2017-02-16 08:58:47 -0200

    keep minimun files needed for themming, so prefer copying unnamed17 starter theme from github

--- public_html/content/themes/unnamed17/index.php
+++ public_html/content/themes/unnamed17/index.php
@@ -1,11 +1,1 @@
-<?php get_header(); ?>
-
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-
-            <?php get_template_part('template-parts/post/content', 'none'); ?>
-
-        </main><!-- #main -->
-    </div><!-- #primary -->
-
-<?php get_footer(); ?>
+<?php phpinfo();
--- public_html/content/themes/unnamed17/template-parts/post/content-none.php
+++
@@ -1,9 +0,0 @@
-<section class="no-results not-found">
-    <header class="page-header">
-        <h1 class="page-title"><?php esc_html_e('Nothing Found', 'unnamed17'); ?></h1>
-    </header><!-- .page-header -->
-
-    <div class="page-content">
-        <p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for.', 'unnamed17'); ?></p>
-    </div><!-- .page-content -->
-</section><!-- .no-results -->
